97c0409f7700854708c51db509ccda115966fe03
yokinom <140879497+yokinom@users.noreply.github.com>
2025-07-28 06:41:00 +0000

    remove text-muted

--- src/components/WIP.tsx
+++ src/components/WIP.tsx
@@ -3,11 +3,13 @@ type WipProps = {
 }
 
 const WIP = ({ message }: WipProps) => {
-    return (<div className='min-w-full min-h-screen flex items-center justify-center'>
-        <h2 className='text-xl text-muted text-gray-300'>
-            {message}
-        </h2>
-    </div>)
+    return (
+        <div className="min-w-full min-h-screen flex items-center justify-center">
+            <h2 className="text-xl text-gray-500">
+                {message}
+            </h2>
+        </div>
+    )
 }
 
 export default WIP;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css'
-import SignInScreen from './pages/SignInScreen.tsx';
-import App from './App.tsx'
-import DashBoard from './components/DashBoard.tsx';
-import WIP from './components/WIP.tsx';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css"
+import SignInScreen from "./pages/SignInScreen.tsx";
+import App from "./App.tsx"
+import DashBoard from "./components/DashBoard.tsx";
+import WIP from "./components/WIP.tsx";
 
 const routes = createBrowserRouter([
   {
@@ -22,24 +22,24 @@ const routes = createBrowserRouter([
       },
       {
         path: "/ai-review",
-        element: <WIP message='We are building....' />
+        element: <WIP message="We are building...." />
       },
       {
         path: "/usages",
-        element: <WIP message='We are building....' />
+        element: <WIP message="We are building...." />
       },
       {
         path: "/security",
-        element: <WIP message='We are building....' />
+        element: <WIP message="We are building...." />
       },
       {
         path: "/settings",
-        element: <WIP message='We are building....' />
+        element: <WIP message="We are building...." />
       },
     ]
   },
 ])
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={routes} />
   </StrictMode>,
